38bf825de9b786163ef886e47a79c240de9337e3
Abdellah Dbib <abdellah.dbib@outlook.com>
2020-07-23 23:19:07 +0100

    Achat update (Front-end)

---
+++ GestionStock/Controllers/StockController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestionStock.Models.Business;
+using GestionStock.Models.Business.Imp;
+using GestionStock.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+
+namespace GestionStock.Controllers
+{
+    public class StockController : Controller
+    {
+        StockBusiness stockBusiness = new StockBusinessImp();
+        // GET: StockController
+        public ActionResult Index()
+        {
+            Utilisateur util = GetChefFromCookie();
+            ViewBag.utilisateur = util;
+            List<Stock> listStock = stockBusiness.getAllStock();
+            return View(listStock);
+        }
+
+        // GET: StockController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StockController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StockController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StockController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StockController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StockController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StockController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //----------------------------------------------------------------------
+        [VerifyUserAttribute]
+        private Utilisateur GetChefFromCookie()
+        {
+            var jsonResult = HttpContext.Session.GetString("Utilisateur");
+
+            return JsonConvert.DeserializeObject<Utilisateur>(jsonResult);
+        }
+        //----------------------------------------------------------------------
+
+        public class VerifyUserAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                var user = filterContext.HttpContext.Session.GetString("Utilisateur");
+                if (user == null)
+                    filterContext.Result = new RedirectResult(string.Format("/Login"));
+            }
+        }
+    }
+}
